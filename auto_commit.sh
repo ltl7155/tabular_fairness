27f58fc1f0eb181b370bd6677ceab52ef6fea9b0
wangjian_dgx <jian.wang@ntu.edu.sg>
2021-08-25 11:00:14 +0000

    Scripted auto-commit on change (2021-08-25 11:00:13) by gitwatch.sh

--- auto_commit.sh
+++ auto_commit.sh
@@ -1,6 +1,4 @@
 cd /mnt/mfs/litl/tabular_fairness/FairnessRepair && \
-sh auto_commit_add.sh 2>&1 >/dev/null  && \
-sh auto_commit_del.sh 2>&1 >/dev/null  && \
 git ci -m 'auto commit' && \
 git push origin main && \
 echo "finish " 
