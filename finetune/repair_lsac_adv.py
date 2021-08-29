723899c17d342f1ab31de6b86088031029dac80b
wangjian_dgx <jian.wang@ntu.edu.sg>
2021-08-29 11:29:34 +0000

    Scripted auto-commit on change (2021-08-29 11:29:34) by gitwatch.sh

--- finetune/repair_lsac_adv.py
+++ finetune/repair_lsac_adv.py
@@ -61,7 +61,7 @@ def construct_model(frozen_layers, attr, adv):
     for i, l in enumerate(layer_lst):
         x = l(x)
     y_income = layer6(x)
-    
+    model = keras.Model(input, [y_income])
         
     y_advs = []
     
