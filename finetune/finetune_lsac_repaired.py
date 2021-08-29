7f57857ffd5cb7542a807e5d5879ef27affa8507
wangjian_dgx <jian.wang@ntu.edu.sg>
2021-08-29 13:51:44 +0000

    Scripted auto-commit on change (2021-08-29 13:51:43) by gitwatch.sh

--- finetune/finetune_lsac_repaired.py
+++ finetune/finetune_lsac_repaired.py
@@ -83,8 +83,8 @@ if __name__ == '__main__':
     frozen_layers = [1, 2, 3, 4, 5]
     
     models_map = {
-        'r': "models/gated_models/adult_g_gated_4_0.3_0.2_p-0.6_p0.1.h5",
         'g': "models/gated_models/adult_r_gated_4_0.3_0.2_p-0.95_p0.8.h5",
+        'r': "models/gated_models/adult_g_gated_4_0.3_0.2_p-0.6_p0.1.h5",
     }
 
     for frozen_layer in frozen_layers:
