c829948d66f7c5411ecb10d5599bdb663c2748ed
wangjian_dgx <jian.wang@ntu.edu.sg>
2021-08-29 10:51:57 +0000

    Scripted auto-commit on change (2021-08-29 10:51:57) by gitwatch.sh

--- finetune/repair_bank_adv.py
+++ finetune/repair_bank_adv.py
@@ -39,7 +39,7 @@ def construct_model(frozen_layers, attr, adv):
     x = input
     for i, l in enumerate(layer_lst):
         x = l(x)
-    # y_income = layer6(x)
+    y_income = layer6(x)
     y = last_layer(x)
     model = keras.Sequential([input, layer1, layer2, layer3, layer4, layer5, last_layer])
     # return keras.Model(input, y_race)
