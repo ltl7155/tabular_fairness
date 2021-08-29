d7ef10e2bd56bef38b9b6eb5904f589ca410e934
wangjian_dgx <jian.wang@ntu.edu.sg>
2021-08-29 09:45:14 +0000

    Scripted auto-commit on change (2021-08-29 09:45:14) by gitwatch.sh

--- finetune/repair_adult_adv.py
+++ finetune/repair_adult_adv.py
@@ -77,11 +77,12 @@ if __name__ == '__main__':
         y_train_labels = {}
         y_val_labels = {}
         last_layer_name = 'layer_' + attr
-        losses[last_layer_name] = 'binary_crossentropy'
+        losses["layer6"] = 'binary_crossentropy'
         if attr == "g":
             losses[last_layer_name] = 'binary_crossentropy'
         else:
             losses[last_layer_name] = 'categorical_crossentropy'
+        losses_weights[last_layer_name] = 1.0
         losses_weights[last_layer_name] = 1.0
         metrics[last_layer_name] = "accuracy"
         if attr == "g":
