ccdfc0b754aa2baef7869c8ec7985e7a71517ba3
wangjian_dgx <jian.wang@ntu.edu.sg>
2021-08-29 13:28:31 +0000

    Scripted auto-commit on change (2021-08-29 13:28:31) by gitwatch.sh

--- finetune/finetune_adult_repaired.py
+++ finetune/finetune_adult_repaired.py
@@ -32,9 +32,11 @@ def construct_model(frozen_layers, attr):
         last_layer = keras.layers.Dense(c, activation="sigmoid", name='layer_' + attr)
     else:
         last_layer = keras.layers.Dense(c, activation="softmax", name='layer_' + attr)
+        
+        
     layer_lst = [layer1, layer2, layer3, layer4, layer5]
-    for layer in layer_lst[0: frozen_layers]:
-        layer.trainable = False
+    
+    layer_lst = [frozen_layers, ]
     x = input
     for i, l in enumerate(layer_lst):
         x = l(x)
