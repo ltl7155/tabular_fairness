3cb4fd5989033f9460f16fc641401b94c3ff2613
wangjian_dgx <jian.wang@ntu.edu.sg>
2021-08-29 09:41:38 +0000

    Scripted auto-commit on change (2021-08-29 09:41:38) by gitwatch.sh

--- finetune/repair_adult_adv.py
+++ finetune/repair_adult_adv.py
@@ -43,7 +43,7 @@ def construct_model(frozen_layers, attr):
     if adv:
         y = last_layer(x)
         
-    model = keras.Sequential([input, layer1, layer2, layer3, layer4, layer5, last_layer])
+    model = keras.Sequential([input, layer1, layer2, layer3, layer4, layer5, layer6])
     
     # return keras.Model(input, y_race)
     return model
