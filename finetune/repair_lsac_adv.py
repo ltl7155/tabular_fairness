b6461ff4eecdf3a2b662fd47994058a31e3ee717
wangjian_dgx <jian.wang@ntu.edu.sg>
2021-08-29 11:08:07 +0000

    Scripted auto-commit on change (2021-08-29 11:08:07) by gitwatch.sh

--- finetune/repair_lsac_adv.py
+++ finetune/repair_lsac_adv.py
@@ -126,10 +126,10 @@ if __name__ == '__main__':
         file_path = '../models/retrained_adv/'
         if not os.path.exists(file_path):
             os.makedirs(file_path)
-        model_name = (file_path + args.attr + '_bank_multi_model_' + str(frozen_layer) + '.h5')
+        model_name = (file_path + args.attr + '_lsac_multi_model_' + str(frozen_layer) + '.h5')
         tf.keras.models.save_model(model, model_name)
 
         saved_model = construct_model(frozen_layer, args.attr, adv=False)
         saved_model.load_weights(model_name, by_name=True)
-        model_name = (file_path + args.attr + '_bank_model_' + str(frozen_layer) + '.h5')
+        model_name = (file_path + args.attr + '_lsac_model_' + str(frozen_layer) + '.h5')
         tf.keras.models.save_model(saved_model, model_name)
