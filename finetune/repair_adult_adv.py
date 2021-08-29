7974bd38623b06120f589bcc137c6beea90a618b
wangjian_dgx <jian.wang@ntu.edu.sg>
2021-08-29 11:20:35 +0000

    Scripted auto-commit on change (2021-08-29 11:20:35) by gitwatch.sh

--- finetune/repair_adult_adv.py
+++ finetune/repair_adult_adv.py
@@ -52,6 +52,7 @@ def construct_model(frozen_layers, attr, adv):
     if adv:
         for attr in attrs:
             y_adv = last_layer(x)
+            y_advs.append(y_adv)
         
         model = keras.Model(input, [y_income, y_adv])
         
