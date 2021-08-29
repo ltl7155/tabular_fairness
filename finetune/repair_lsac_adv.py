f69a4393d9df20eb4fcb060f6cddfa073bc86bc9
wangjian_dgx <jian.wang@ntu.edu.sg>
2021-08-29 11:28:03 +0000

    Scripted auto-commit on change (2021-08-29 11:28:03) by gitwatch.sh

--- finetune/repair_lsac_adv.py
+++ finetune/repair_lsac_adv.py
@@ -47,11 +47,12 @@ def construct_model(frozen_layers, attr, adv):
     layer5 = keras.layers.Dense(5, activation="relu", name="layer5")
     layer6 = keras.layers.Dense(1, activation="sigmoid", name="layer6")
     if adv:
-        c = category_map[attr]
-        if attr == 'g':
-            last_layer = keras.layers.Dense(c, activation="sigmoid", name='layer_' + attr)
-        else:
-            last_layer = keras.layers.Dense(c, activation="softmax", name='layer_' + attr)
+        for attr in attrs:
+            c = category_map[attr]
+            if attr == 'g':
+                last_layer = keras.layers.Dense(c, activation="sigmoid", name='layer_' + attr)
+            else:
+                last_layer = keras.layers.Dense(c, activation="softmax", name='layer_' + attr)
     layer_lst = [layer1, layer2, layer3, layer4, layer5]
     for layer in layer_lst[0: frozen_layers]:
         layer.trainable = False
