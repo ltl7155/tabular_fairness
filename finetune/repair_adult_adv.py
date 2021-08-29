8ede880c6127cc6d7a24d64eed02380ca3a4939d
wangjian_dgx <jian.wang@ntu.edu.sg>
2021-08-29 09:40:17 +0000

    Scripted auto-commit on change (2021-08-29 09:40:16) by gitwatch.sh

--- finetune/repair_adult_adv.py
+++ finetune/repair_adult_adv.py
@@ -27,7 +27,7 @@ def construct_model(frozen_layers, attr):
     layer3 = keras.layers.Dense(15, activation="relu", name="layer3")
     layer4 = keras.layers.Dense(15, activation="relu", name="layer4")
     layer5 = keras.layers.Dense(10, activation="relu", name="layer5")
-    # layer6 = keras.layers.Dense(1, activation="sigmoid", name="layer6")
+    layer6 = keras.layers.Dense(1, activation="sigmoid", name="layer6")
     c = category_map[attr]
     if attr == 'g':
         last_layer = keras.layers.Dense(c, activation="sigmoid", name='layer_' + attr)
