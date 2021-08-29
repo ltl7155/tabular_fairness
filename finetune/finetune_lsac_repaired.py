13c4d2f93f401fb90761c222d6950fd4f233914a
wangjian_dgx <jian.wang@ntu.edu.sg>
2021-08-29 13:50:03 +0000

    Scripted auto-commit on change (2021-08-29 13:50:03) by gitwatch.sh

--- finetune/finetune_lsac_repaired.py
+++ finetune/finetune_lsac_repaired.py
@@ -53,8 +53,7 @@ def construct_model(frozen_layers, attr):
         last_layer = keras.layers.Dense(c, activation="softmax", name='layer_' + attr)
     layer_lst = [layer1, layer2, layer3, layer4, layer5]
     layer_lst = layer_lst[frozen_layers:]
-    for layer in layer_lst[0: frozen_layers]:
-        layer.trainable = False
+    
     x = input
     for i, l in enumerate(layer_lst):
         x = l(x)
