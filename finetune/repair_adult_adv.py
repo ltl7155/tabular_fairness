f4fd7551ef959699673ef3bced14d4cec9047d30
wangjian_dgx <jian.wang@ntu.edu.sg>
2021-08-29 12:38:37 +0000

    Scripted auto-commit on change (2021-08-29 12:38:36) by gitwatch.sh

--- finetune/repair_adult_adv.py
+++ finetune/repair_adult_adv.py
@@ -86,7 +86,6 @@ if __name__ == '__main__':
     y_train_labels = {}
     y_val_labels = {}
     for attr in attrs:
-        
         last_layer_name = 'layer_' + attr
         losses_weights[last_layer_name] = -1.0
         metrics[last_layer_name] = "accuracy"
