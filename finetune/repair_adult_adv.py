e1a044fafce941d7e379d613adf332f52e9fee33
wangjian_dgx <jian.wang@ntu.edu.sg>
2021-08-30 08:17:10 +0000

    Scripted auto-commit on change (2021-08-30 08:17:09) by gitwatch.sh

--- finetune/repair_adult_adv.py
+++ finetune/repair_adult_adv.py
@@ -123,7 +123,7 @@ if __name__ == '__main__':
     history = model.fit(x=X_train, y=y_train_labels, epochs=10, validation_data=(X_val, y_val_labels))
 
     # save model.
-    file_path = '../models/retrained_adv/'
+    file_path = 'models/retrained_adv/'
     if not os.path.exists(file_path):
         os.makedirs(file_path)
     model_name = (file_path + args.attr + '_adult_multi_model_' + str(frozen_layer) + '.h5')
