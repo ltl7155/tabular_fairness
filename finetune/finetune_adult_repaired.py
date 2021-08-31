3c3b7e8cba7fa34e5e38f59f6b8391a5aa309ece
wangjian_dgx <jian.wang@ntu.edu.sg>
2021-08-31 04:58:56 +0000

    Scripted auto-commit on change (2021-08-31 04:58:56) by gitwatch.sh

--- finetune/finetune_adult_repaired.py
+++ finetune/finetune_adult_repaired.py
@@ -88,7 +88,8 @@ if __name__ == '__main__':
     for frozen_layer in frozen_layers:
         model_path = models_map[args.attr]
         base_model = keras.models.load_model(model_path, custom_objects={'ScaleLayer': ScaleLayer})
-        layer_name = "scale_layer_" + str(frozen_layer+4)
+#         layer_name = "scale_layer_" + str(frozen_layer+4)
+        layer_name = "layer" + str(frozen_layer+4)
         inner_model = Model(base_model.input, base_model.get_layer(layer_name).output)                                 
         inner_output_train = inner_model.predict(pre_census_income.X_train)
         inner_output_val = inner_model.predict(pre_census_income.X_val)
