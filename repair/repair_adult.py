cfdf47d477e08535bd7bfbdf29f55aa10eb03130
wangjian_dgx <jian.wang@ntu.edu.sg>
2021-08-31 12:03:33 +0000

    Scripted auto-commit on change (2021-08-31 12:03:32) by gitwatch.sh

--- repair/repair_adult.py
+++ repair/repair_adult.py
@@ -280,7 +280,10 @@ if __name__ == '__main__':
         for k in para_res.keys():
             print(k, para_res[k])
             # weights = new_model.get_weights()
-            file_path = f'records/adult_repair/{args.attr}_{args.percent}_{args.weight_threshold}/'
+            prefix = ""
+            if 
+            
+            file_path = f'records/adult_repair{prefix}/{args.attr}_{args.percent}_{args.weight_threshold}/'
             if not os.path.exists(file_path):
                 os.makedirs(file_path)
             file_name = file_path + f'{round(para_res[k][0], 4)}_{round(para_res[k][1], 4)}_{k}.txt'
