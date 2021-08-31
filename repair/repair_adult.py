6ea0f56ba751063ae86dc51e8efe0170427c871b
wangjian_dgx <jian.wang@ntu.edu.sg>
2021-08-31 12:05:16 +0000

    Scripted auto-commit on change (2021-08-31 12:05:16) by gitwatch.sh

--- repair/repair_adult.py
+++ repair/repair_adult.py
@@ -78,7 +78,7 @@ def construct_model(neurons, top_layer, name, min, max, need_weights=True):
         d = ds[i]
         for m in neg:
             if args.ablation !=1 and args.ablation != 3:
-            w = tf.math.add(w, d.weights[0][0][m])
+                w = tf.math.add(w, d.weights[0][0][m])
         for n in pos:
             w = tf.math.subtract(w, d.weights[0][0][n])
     new_w = tf.identity(tf.reshape(w, [1, 1]), name=name)
