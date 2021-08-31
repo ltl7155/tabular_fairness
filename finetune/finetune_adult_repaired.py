c0954c649cbc8dbda195fc42040f2deef890ebc0
wangjian_dgx <jian.wang@ntu.edu.sg>
2021-08-31 03:59:54 +0000

    Scripted auto-commit on change (2021-08-31 03:59:53) by gitwatch.sh

--- finetune/finetune_adult_repaired.py
+++ finetune/finetune_adult_repaired.py
@@ -73,7 +73,7 @@ if __name__ == '__main__':
 #     frozen_layers = [4]
     
     models_map = {
-        'a': "models/gated_models/adult_a_gated_4_0.3_0.2_p-0.3_p0.15.h5",
+        'a': "models/adult_model.h5",
         'r': "models/gated_models/adult_g_gated_4_0.3_0.2_p-0.6_p0.1.h5",
         'g': "models/gated_models/adult_r_gated_4_0.3_0.2_p-0.95_p0.8.h5",
             
