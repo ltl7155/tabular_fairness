ced5d7cd88151373bf4fe8ac1342bbe289d49ef7
wangjian_dgx <jian.wang@ntu.edu.sg>
2021-09-01 14:15:45 +0000

    Scripted auto-commit on change (2021-09-01 14:15:45) by gitwatch.sh

--- repair/repair_bank_ablation.py
+++ repair/repair_bank_ablation.py
@@ -223,7 +223,7 @@ def retrain(k, ps, neurons, para_res):
         repair_acc = 0
 
     finals.append((test_acc, repair_acc))
-    para_res[ps] = (test_acc, repair_acc)
+    para_res[str(ps)] = (test_acc, repair_acc)
 
     if args.saved:
         # model_name = 'models/race_gated_'+str(top_n)+'_'+str(args.percent)+'_'+str(args.weight_threshold)+'.h5'
