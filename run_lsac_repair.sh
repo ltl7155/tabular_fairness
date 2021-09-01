a2beea08be47bf68d0208a7edf02129b21809c7e
wangjian_dgx <jian.wang@ntu.edu.sg>
2021-09-01 14:59:45 +0000

    Scripted auto-commit on change (2021-09-01 14:59:45) by gitwatch.sh

--- run_lsac_repair.sh
+++ run_lsac_repair.sh
@@ -24,7 +24,7 @@ do
             done
             sleep 1m
             wait
-            echo -n "$[j] is finished!";
+            echo -n "$attr is finished!";
         done
         sleep 5m
         wait
