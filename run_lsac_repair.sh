73155e7545b092fbc30954a92f03811488b9fd98
wangjian_dgx <jian.wang@ntu.edu.sg>
2021-09-01 14:33:16 +0000

    Scripted auto-commit on change (2021-09-01 14:33:16) by gitwatch.sh

--- run_lsac_repair.sh
+++ run_lsac_repair.sh
@@ -1,12 +1,28 @@
-for ((i=1;i<21;i++))
+# for ((i=1;i<21;i++))
+# do
+#   for ((j=1;j<20;j++))
+#   do
+# #    CUDA_VISIBLE_DEVICES=$[j%8] nohup python repair_adult.py --attr a\&g --p0 $[i] --p1 $[j] --acc_lb 0.825 2>&1 >adult_a\&g.log  &
+#      CUDA_VISIBLE_DEVICES=$[j%8] nohup python repair/repair_lsac.py --attr g\&r --p0 $[i] --p1 $[j] --acc_lb 0.5 --percent 0.3 --weight_threshold 0.2 2>&1 >backup/lsac_a\&r.log &
+#   done
+#   sleep 2m
+#   wait
+#   echo -n "$[i] is finished!";
+# done
+array=('a')
+per=(0.1,0.2,0.3,0.4,0.5)
+wei=(0.1,0.2,0.3,0.4,0.5)
+for p in ${per[@]}
 do
-  for ((j=1;j<20;j++))
-  do
-#    CUDA_VISIBLE_DEVICES=$[j%8] nohup python repair_adult.py --attr a\&g --p0 $[i] --p1 $[j] --acc_lb 0.825 2>&1 >adult_a\&g.log  &
-     CUDA_VISIBLE_DEVICES=$[j%8] nohup python repair/repair_lsac.py --attr g\&r --p0 $[i] --p1 $[j] --acc_lb 0.5 --percent 0.3 --weight_threshold 0.2 2>&1 >backup/lsac_a\&r.log &
-  done
-  sleep 2m
-  wait
-  echo -n "$[i] is finished!";
+    for w in ${wei[@]}
+    do
+        for attr in ${array[@]}
+        do
+            for ((j=1;j<4;j++))
+            do
+                CUDA_VISIBLE_DEVICES=$[j%7 + 1] nohup python repair/repair_bank_ablation.py --attr $attr --ablation $j --percent $p --weight_threshold $w 2>&1 >adult_a\&r.log &
+            done
+        sleep 2m
+        done
+    done
 done
-
