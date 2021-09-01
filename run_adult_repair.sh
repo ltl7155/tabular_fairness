b194063fd37375bfd7f0e2d8e0b5bceb1cafbc56
wangjian_dgx <jian.wang@ntu.edu.sg>
2021-09-01 02:53:55 +0000

    Scripted auto-commit on change (2021-09-01 02:53:51) by gitwatch.sh

--- run_adult_repair.sh
+++ run_adult_repair.sh
@@ -3,7 +3,7 @@ do
   for ((j=1;j<20;j++))
   do
 #    CUDA_VISIBLE_DEVICES=$[j%8] nohup python repair_adult.py --attr a\&g --p0 $[i] --p1 $[j] --acc_lb 0.825 2>&1 >adult_a\&g.log  &
-     CUDA_VISIBLE_DEVICES=$[j%8] nohup python repair/repair_adult.py --attr r  --p0 $[i] --p1 $[j] --ablation 0 --percent 0.3 --weight_threshold 0.5 2>&1 >adult_a\&r.log &
+     CUDA_VISIBLE_DEVICES=$[j%8] nohup python repair/repair_adult.py --attr r  --p0 $[i] --p1 $[j] --ablation 0 --percent 0.9 --weight_threshold 0.8 2>&1 >adult_a\&r.log &
   done
   sleep 2m
   wait
