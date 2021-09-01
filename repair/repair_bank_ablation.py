997df521ed2fbd590173e692285f51542f717c43
wangjian_dgx <jian.wang@ntu.edu.sg>
2021-09-01 14:00:26 +0000

    Scripted auto-commit on change (2021-09-01 14:00:26) by gitwatch.sh

--- repair/repair_bank_ablation.py
+++ repair/repair_bank_ablation.py
@@ -284,7 +284,7 @@ if __name__ == '__main__':
         for k in para_res.keys():
             print(k, para_res[k])
             prefix = "_ablation" + str(args.ablation)
-            file_path = f'records/adult_repair{prefix}/{args.attr}_{args.percent}_{args.weight_threshold}/'
+            file_path = f'records/bank_repair{prefix}/{args.attr}_{args.percent}_{args.weight_threshold}/'
             if not os.path.exists(file_path):
                 os.makedirs(file_path)
             file_name = file_path + f'{round(para_res[k][0], 4)}_{round(para_res[k][1], 4)}_{k}.txt'
