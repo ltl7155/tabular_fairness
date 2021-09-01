1a7c9f0a580af8dcab397cd986d388b982a00d59
wangjian_dgx <jian.wang@ntu.edu.sg>
2021-09-01 15:03:12 +0000

    Scripted auto-commit on change (2021-09-01 15:03:11) by gitwatch.sh

--- run_german_repair.sh
+++ run_german_repair.sh
@@ -1,12 +1,12 @@
-for ((i=1;i<21;i++))
-do
-  for ((j=1;j<20;j++))
-  do
-      CUDA_VISIBLE_DEVICES=$[j%7+1] nohup python repair/repair_german.py --attr g\&a --p0 $[i] --p1 $[j] --acc_lb 0.7 --percent 0.3 --weight_threshold 0.2 2>&1 >german.log &
-#      CUDA_VISIBLE_DEVICES=$[j-1] nohup  python repair_german.py --attr g --p0 $[i] --p1 $[j] --acc_lb 0.7 --percent 0.3 --weight_threshold 0.2 2>&1 >german.log &
-#      CUDA_VISIBLE_DEVICES=$[j%8] nohup  python repair_german.py --attr g\&a --p0 $[i] --p1 $[j] --acc_lb 0.7 --percent 0.3 --weight_threshold 0.2 2>&1 >german.log &
-  done
-  sleep 2m
-  wait
-  echo -n "$[i] is finished!";
-done
+# for ((i=1;i<21;i++))
+# do
+#   for ((j=1;j<20;j++))
+#   do
+#       CUDA_VISIBLE_DEVICES=$[j%7+1] nohup python repair/repair_german.py --attr g\&a --p0 $[i] --p1 $[j] --acc_lb 0.7 --percent 0.3 --weight_threshold 0.2 2>&1 >german.log &
+# #      CUDA_VISIBLE_DEVICES=$[j-1] nohup  python repair_german.py --attr g --p0 $[i] --p1 $[j] --acc_lb 0.7 --percent 0.3 --weight_threshold 0.2 2>&1 >german.log &
+# #      CUDA_VISIBLE_DEVICES=$[j%8] nohup  python repair_german.py --attr g\&a --p0 $[i] --p1 $[j] --acc_lb 0.7 --percent 0.3 --weight_threshold 0.2 2>&1 >german.log &
+#   done
+#   sleep 2m
+#   wait
+#   echo -n "$[i] is finished!";
+# done
