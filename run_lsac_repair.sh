493511c0c0d034b6cceec1c3f9a3508547e07cc8
wangjian_dgx <jian.wang@ntu.edu.sg>
2021-09-01 15:00:49 +0000

    Scripted auto-commit on change (2021-09-01 15:00:48) by gitwatch.sh

--- run_lsac_repair.sh
+++ run_lsac_repair.sh
@@ -20,6 +20,7 @@ do
         do
             for ((j=1;j<4;j++))
             do
+            echo $attr
                 CUDA_VISIBLE_DEVICES=$[j%7 + 1] nohup python repair/repair_lsac_ablation.py --attr $attr --ablation $j --percent $p --weight_threshold $w 2>&1 >adult_a\&r.log &
             done
             sleep 1m
