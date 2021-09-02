b0f1e0a32598e599cedcbbf85440b9aedf1b1b15
wangjian_dgx <jian.wang@ntu.edu.sg>
2021-09-02 03:39:24 +0000

    Scripted auto-commit on change (2021-09-02 03:39:23) by gitwatch.sh

--- repair/repair_lsac.py
+++ repair/repair_lsac.py
@@ -231,7 +231,7 @@ def retrain(k, ps, neurons, para_res):
 pos_map = { 
             'r': [10],
             'g': [9],
-            'r&g': [10, 9]
+            'g&r': [9, 10]
             }
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='fine-tune models with protected attributes')
