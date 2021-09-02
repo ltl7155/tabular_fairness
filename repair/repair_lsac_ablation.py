653ca7c4859a6920e02ec7bcd7ef00e109676abc
wangjian_dgx <jian.wang@ntu.edu.sg>
2021-09-02 03:38:46 +0000

    Scripted auto-commit on change (2021-09-02 03:38:45) by gitwatch.sh

--- repair/repair_lsac_ablation.py
+++ repair/repair_lsac_ablation.py
@@ -236,7 +236,7 @@ def retrain(k, ps, neurons, para_res):
 pos_map = { 
             'r': [10],
             'g': [9],
-            'g&r': [10, 9]
+            'r&g': [10, 9]
             }
 paras_map = { 
             'r': [-0.9, 0.05],
